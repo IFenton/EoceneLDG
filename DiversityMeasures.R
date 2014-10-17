aa7fcdf79ce28382a0733b942e98fab1f536645e
Isabel Fenton <isabf@nhm.ac.uk>
2014-10-17 15:07:57 +0100

    Some changes

--- DiversityMeasures.R
+++ DiversityMeasures.R
@@ -13,7 +13,7 @@
 
 setwd("C:/Documents/Science/PhD/Project/Eocene/")
 
-# 1. Load in the data -----------------------------------------------------
+## 1. Load in the data -----------------------------------------------------
 load("C:/Documents/Science/PhD/Project/Eocene/Outputs/OlComb.RData")
 load("C:/Documents/Science/PhD/Project/Eocene/Outputs/EocComb.RData")
 
@@ -28,9 +28,9 @@ EO_abun <- EO_abun[EO_abun$All != "No", ]
 dim(EO_abun)
 
 
-# 2. Calculate species richness -------------------------------------------
+## 2. Calculate species richness -------------------------------------------
 
-# 2a. Generate a dataframe with each row as a site at a given age ---------
+## 2a. Generate a dataframe with each row as a site at a given age ---------
 # create a dataframe containing rows for each unique site / age
 # add column of site ID / age for sorting
 EO_abun$site.age <- NA
@@ -51,7 +51,7 @@ EO_abun_div <- EO_abun[!duplicated(EO_abun$site.age), ]
 # remove rows for species and abundance
 EO_abun_div <- EO_abun_div[, -c(which(names(EO_abun_div) == "Species"), which(names(EO_abun_div) == "Abundance"))]
 
-# 2b. Calculate the number of species at each site for each age -----------
+## 2b. Calculate the number of species at each site for each age -----------
 EO_abun_div$SR <- NA
 for(i in 1:nrow(EO_abun_div)) {
   EO_abun_div$SR[i] <- sum(EO_abun$site.age == EO_abun_div$site.age[i])
@@ -65,7 +65,7 @@ for (i in unique(EO_abun_div$Period)) {
   with(EO_abun_div[EO_abun_div$Period == i, ], plot(Latitude, SR, pch = 16, col = as.factor(Period), main = i))
 }
 
-# 2c. Generate a dataframe with sites by period ---------------------------
+## 2c. Generate a dataframe with sites by period ---------------------------
 # create a dataframe of average richness by period
 EO_abun_div$site.period <- NA
 for (i in 1:nrow(EO_abun_div)) {
@@ -77,13 +77,13 @@ EO_abun_age <- EO_abun_div[!duplicated(EO_abun_div$site.period), ]
 # remove sprich column
 EO_abun_age <- EO_abun_age[, -21]
 
-# 2d. Calculate average species richness for each site --------------------
+## 2d. Calculate average species richness for each site --------------------
 EO_abun_age$SR <- NA
 for(i in 1:nrow(EO_abun_age)) {
   EO_abun_age$SR[i] <- mean(EO_abun_div$SR[which(EO_abun_div$site.period == EO_abun_age$site.period[i])])
 }
 
-# 2e. Plot this up --------------------------------------------------------
+## 2e. Plot this up --------------------------------------------------------
 
 with(EO_abun_age, plot(Latitude, SR, pch = 16, col = as.factor(Period)))
 par(ask = TRUE)
@@ -94,7 +94,7 @@ for (i in unique(EO_abun_age$Period)) {
 par(ask = FALSE)
 
 
-# species richness by latitude split by period -------------
+## species richness by latitude split by period -------------
 par(mfrow = c(2, 3))
 with(EO_abun_age[EO_abun_age$Period == "Lower Eocene", ], plot(pal.lat.m, SR, pch = 16, col = as.factor(Period), main = "Lower Eocene"))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene", ], plot(pal.lat.m, SR, pch = 16, col = as.factor(Period), main = "Middle Eocene"))
@@ -103,7 +103,7 @@ with(EO_abun_age[EO_abun_age$Period == "Lower Oligocene", ], plot(pal.lat.m, SR,
 with(EO_abun_age[EO_abun_age$Period == "Upper Oligocene", ], plot(pal.lat.m, SR, pch = 16, col = as.factor(Period), main = "Upper Oligocene"))
 
 
-# species richness by latitude split by hemisphere and period -------------
+## species richness by latitude split by hemisphere and period -------------
 par(mfrow = c(2, 3))
 with(EO_abun_age[EO_abun_age$Period == "Lower Eocene", ], plot(abs(pal.lat.m), SR, pch = 16, col = (abs(pal.lat.m) == pal.lat.m) + 1, main = "Lower Eocene"))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene", ], plot(abs(pal.lat.m), SR, pch = 16, col = (abs(pal.lat.m) == pal.lat.m) + 1, main = "Middle Eocene"))
@@ -112,7 +112,7 @@ with(EO_abun_age[EO_abun_age$Period == "Lower Oligocene", ], plot(abs(pal.lat.m)
 with(EO_abun_age[EO_abun_age$Period == "Upper Oligocene", ], plot(abs(pal.lat.m), SR, pch = 16, col = (abs(pal.lat.m) == pal.lat.m) + 1, main = "Upper Oligocene"))
 
 
-# species richness with regression fitted by period -----------------------
+## species richness with regression fitted by period -----------------------
 for (i in unique(EO_abun_age$Period)) {
   with(EO_abun_age[EO_abun_age$Period == i, ], plot(abs(pal.lat.m), SR, pch = 16, col = as.factor(Period), main = i))
   abline(lm(SR ~ pal.lat.m, data = EO_abun_age[EO_abun_age$Period == i, ]))
@@ -121,7 +121,7 @@ for (i in unique(EO_abun_age$Period)) {
 head(EO_abun_age$site.period)
 
 
-# species richness by latitude plotted by period --------------------------
+## species richness by latitude plotted by period --------------------------
 par(mfrow = c(2, 3))
 with(EO_abun_age[EO_abun_age$Period == "Lower Eocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = All))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = All))
@@ -130,7 +130,7 @@ with(EO_abun_age[EO_abun_age$Period == "Lower Oligocene" & EO_abun_age$All != "Y
 with(EO_abun_age[EO_abun_age$Period == "Upper Oligocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = All))
 
 
-# species richness against latitude by period, coloured by hemisphere --------------
+## species richness against latitude by period, coloured by hemisphere --------------
 par(mfrow = c(2, 3))
 with(EO_abun_age[EO_abun_age$Period == "Lower Eocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = (pal.lat.m > 0) + 1))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = (pal.lat.m > 0) + 1))
@@ -139,14 +139,14 @@ with(EO_abun_age[EO_abun_age$Period == "Lower Oligocene" & EO_abun_age$All != "Y
 with(EO_abun_age[EO_abun_age$Period == "Upper Oligocene" & EO_abun_age$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = (pal.lat.m > 0) + 1))
 
 
-# distribution maps of species richness by period -------------------------
+## distribution maps of species richness by period -------------------------
 with(EO_abun_age[EO_abun_age$Period == "Lower Eocene", ], distrib.map(mod_long51, mod_lat51, SR))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene", ], distrib.map(mod_long42, mod_lat42, SR))
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene", ], distrib.map(mod_long39, mod_lat39, SR))
-with(EO_abun_age[EO_abun_adge$Period == "Upper Eocene", ], distrib.map(mod_long35, mod_lat35, SR))
+with(EO_abun_age[EO_abun_age$Period == "Upper Eocene", ], distrib.map(mod_long35, mod_lat35, SR))
 
 
-# plot map of points at 39Ma ----------------------------------------------
+## plot map of points at 39Ma ----------------------------------------------
 PTR.39Mya <- readPNG("Output\\39Mya.png")
 PTR.39Mya.img <- pixmapGrey(PTR.39Mya)
 plot(PTR.39Mya.img)
@@ -158,13 +158,13 @@ points((180 + 180) / 360 * 1802.6 + 135.8, (90 + 90) /180 * 903.1  + 202, pch = 
 with(EO_abun_age[EO_abun_age$Period == "Middle Eocene",], points((mod_long39 + 180) / 360 * 1802.6 + 135.8, (mod_lat39 + 90) / 180 * 903.1  + 202, pch = 16, col = "black"))
 
 
-#  create a smaller dataset -----------------------------------------------
+##  create a smaller dataset -----------------------------------------------
 # remove all almost and those with poor preservation
 EO_abun_sub <- EO_abun_age[EO_abun_age$All != "Yes (almost)" &EO_abun_age$Preservation != 3 & EO_abun_age$Preservation != 4, ]
 table(EO_abun_age$Preservation) # remove 3 and 4
 
 
-# species richness in eocene ages with average maximum SR --------------------------------
+## species richness in eocene ages with average maximum SR --------------------------------
 
 par(mfrow = c(1, 3))
 with(EO_abun_sub[EO_abun_sub$Period == "Lower Eocene" & EO_abun_sub$All != "Yes (almost)" , ], plot(abs(pal.lat.m), SR, pch = 16, col = (pal.lat.m > 0) + 1))
@@ -190,7 +190,7 @@ for (i in 1:7) {
 points(seq(5, 65, 10), mn.up, type = "l")
 
 
-# species richness in eocene ages with regression fitted ----------------------------
+## species richness in eocene ages with regression fitted ----------------------------
 
 par(mfrow = c(1, 3))
 with(EO_abun_sub[EO_abun_sub$Period == "Lower Eocene" & EO_abun_sub$All != "Yes (almost)" , ], plot(abs(pal.lat.m), SR, pch = 16, col = (pal.lat.m > 0) + 1))
@@ -207,13 +207,11 @@ modU <- with(EO_abun_sub[EO_abun_sub$Period == "Upper Eocene", ], lm(SR ~ poly(a
 points(0:70, predict(modU, data.frame(pal.lat.m = 0:70)), type = "l")
 
 
-# CBEP --------------------------------------------------------------------
+## CBEP --------------------------------------------------------------------
 
-# plot only those that are yes and preservation of 0 or n/a
+## plot only those that are yes and preservation of 0 or n/a
 
-
-
-# species richness by lat coloured by All? --------------------------------
+## species richness by lat coloured by All? --------------------------------
 par(mfrow = c(1, 3))
 with(EO_abun_sub[EO_abun_sub$Period == "Lower Eocene" & EO_abun_sub$All != "Yes (almost)" , ], plot(abs(pal.lat.m), SR, pch = 16, col = All))
 
@@ -221,7 +219,7 @@ with(EO_abun_sub[EO_abun_sub$Period == "Middle Eocene" & EO_abun_sub$All != "Yes
 
 with(EO_abun_sub[EO_abun_sub$Period == "Upper Eocene" & EO_abun_sub$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = All))
 
-# species richness by lat coloured by Preservation? --------------------------------
+## species richness by lat coloured by Preservation? --------------------------------
 par(mfrow = c(1, 3))
 with(EO_abun_sub[EO_abun_sub$Period == "Lower Eocene" & EO_abun_sub$All != "Yes (almost)" , ], plot(abs(pal.lat.m), SR, pch = 16, col = Preservation))
 
@@ -230,7 +228,7 @@ with(EO_abun_sub[EO_abun_sub$Period == "Middle Eocene" & EO_abun_sub$All != "Yes
 with(EO_abun_sub[EO_abun_sub$Period == "Upper Eocene" & EO_abun_sub$All != "Yes (almost)", ], plot(abs(pal.lat.m), SR, pch = 16, col = Preservation))
 
 
-# genus level richness ----------------------------------------------------
+## genus level richness ----------------------------------------------------
 summary(EO_abun_div$Preservation)
 
 # remove all poor preservation
@@ -289,7 +287,7 @@ with(EO_abun_CBEP[EO_abun_CBEP$Period == "Upper Eocene", ], plot(abs(pal.lat.m),
 
 
 
-# calculating maximum diversity for a given time period -------------------
+## calculating maximum diversity for a given time period -------------------
 # get a dataset which is the unique values of age & lat & long
 names(EO_abun_CBEP)
 max.abun <- EO_abun_CBEP[!duplicated(EO_abun_CBEP[, c(3:4, 7)]), ] 
@@ -318,10 +316,10 @@ points(0:70, predict(modU, data.frame(pal.lat.m = 0:70)), type = "l")
 with(max.abun[max.abun$Period == "Upper Eocene", ], anova(modU, lm(SR ~ 1)))
 
 
-# 3. Calculate evenness ---------------------------------------------------
+## 3. Calculate evenness ---------------------------------------------------
 
 
-# 4. Calculate lineage ages --------------------------------------------------
+## 4. Calculate lineage ages --------------------------------------------------
 
 
 
